93c8702c0dc021015cd19832f7b4c154f9a0b986
Edgar.yjb <184515560@qq.com>
2016-07-20 10:30:19 +0800

    正则表达式中的方括号

--- javascript正则表达式/test.js
+++ javascript正则表达式/test.js
@@ -3,9 +3,5 @@
  */
 
 
-var str = "longen and longen and yunxi";
-console.log(/longen/g.exec(str));
-// 打印 ["longen", index: 0, input: "longen and yunxi"]
-
-// 假如没有找到的话，则返回null
-console.log(/wo/.exec(str)); // null
+var str = "abcde";
+console.log(str.match(/[bcd][bcd]/)); // [“bc”, index: 1, input: “abcde”]
