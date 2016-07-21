4b8548c89b13e0012625d285eb2b2e24a83d0e53
Edgar.yjb <184515560@qq.com>
2016-07-21 10:01:05 +0800

    第一次只出现一次的字符

--- 经典算法题/剑指offer/35-第一个只出现一次的字符.js
+++ 经典算法题/剑指offer/35-第一个只出现一次的字符.js
@@ -1,3 +1,33 @@
 /**
- * Created by Edgar.yjb on 16/7/21.
- */
+ * 第一个只出现一次的字符
+ * */
+
+function firstOnlyOne(str) {
+    if (!str) {
+        return null;
+    }
+
+    var a = [];
+
+    for (var i = 0, l = str.length; i < l; i++) {
+        var ele = str[i];
+        var index = a.indexOf(ele);
+
+        if (index === -1) {
+            a.push(ele);
+        } else {
+            a.splice(index, 1);
+        }
+    }
+
+    if (a.length) {
+        return a[0];
+        // return a;  //找出所有只出现一次的字符
+    }
+    return null;
+}
+
+console.log(firstOnlyOne(''));
+console.log(firstOnlyOne('hello'));
+console.log(firstOnlyOne('hhaay'));
+console.log(firstOnlyOne('hhaayy'));
