05d04c1d7919b472fb688dbf3858d97a936572c0
Edgar.yjb <184515560@qq.com>
2016-08-26 09:50:11 +0800

    首个重复字符

---
+++ 08-数据结构与算法/05-常见面试算法题/2016校招编程真题(!!!)/05-去哪儿-首个重复字符.js
@@ -0,0 +1,34 @@
+/**
+ * 题目描述
+
+ 对于一个字符串，请设计一个高效算法，找到第一次重复出现的字符。
+ 给定一个字符串(不一定全为字母)A及它的长度n。请返回第一个重复出现的字符。
+ 保证字符串中有重复字符，字符串的长度小于等于500。
+
+ 测试样例：
+ "qywyer23tdd",11
+
+ 返回：y
+ * */
+
+
+function findFirstRepeat(str) {
+    // write code here
+
+    var obj = {};
+
+    for (var i = 0, l = str.length; i < l; i++) {
+        var ch = str[i];
+
+        if (!obj.hasOwnProperty(ch)) {
+            obj[ch] = 1;
+        } else {
+            return ch;
+        }
+    }
+
+    return null;
+}
+
+console.log(findFirstRepeat('qywyer23tdd'));
+console.log(findFirstRepeat('abcdefgabc'));
