4e114f8695a1eac07f20ac894afd7e381343237d
Edgar.yjb <184515560@qq.com>
2016-09-12 10:46:34 +0800

    替换空格

--- 08-数据结构与算法/04-剑指offer全部题解!!/000-剑指offer题目js代码实现.js
+++ 08-数据结构与算法/04-剑指offer全部题解!!/000-剑指offer题目js代码实现.js
@@ -1,28 +1,62 @@
 //03. 二维数组中的查找
+/*
+ function findNumber(arr, number) {
+ var i = 0, j = arr[0].length;
 
-function findNumber(arr, number) {
-    var i = 0, j = arr[0].length;
+ while (i < arr.length && j >= 0) {
+ if (arr[i][j] === number) {
+ return [i, j];
+ } else if (arr[i][j] < number) {
+ i++;
+ } else {
+ j--;
+ }
+ }
 
-    while (i < arr.length && j >= 0) {
-        if (arr[i][j] === number) {
-            return [i, j];
-        } else if (arr[i][j] < number) {
-            i++;
-        } else {
-            j--;
+ return [-1, -1];  //not found
+ }
+
+
+ var arr = [
+ [1, 2, 8, 9],
+ [2, 4, 9, 12],
+ [4, 7, 10, 13],
+ [6, 8, 11, 15]
+ ];
+
+ console.log(findNumber(arr, 15));
+ console.log(findNumber(arr, 65));*/
+
+
+// 04. 替换空格
+/*
+function replaceSpaces(str) {
+    var arr = str.split('');
+    var spaceCount = 0;
+    var len = arr.length;
+
+    //先遍历一次,得到空格字符的个数
+    for (var i = 0, l = len; i < l; i++) {
+        if (arr[i] === ' ') {
+            spaceCount += 2;
         }
     }
 
-    return [-1, -1];  //not found
+    //从后往前遍历,避免元素的多次移动
+    var totalLength = len + spaceCount;
+    var index = totalLength - 1;  //从后往前的指针
+    for (var j = len - 1; j >= 0; j--) {
+        if (arr[j] !== ' ') {
+            arr[index] = arr[j];
+            index--;
+        } else {
+            arr[index--] = '0';
+            arr[index--] = '2';
+            arr[index--] = '%';
+        }
+    }
+
+    return arr.join('');
 }
 
-
-var arr = [
-    [1, 2, 8, 9],
-    [2, 4, 9, 12],
-    [4, 7, 10, 13],
-    [6, 8, 11, 15]
-];
-
-console.log(findNumber(arr, 15));
-console.log(findNumber(arr, 65));
+console.log(replaceSpaces('This is a sentence for testing.'));*/
