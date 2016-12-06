62fec4ee56374a5593d7f92efa494d44acfb076c
Edgar.yjb <184515560@qq.com>
2016-12-06 20:57:09 +0800

    字符串与数组

--- testtttttt.js
+++ testtttttt.js
@@ -1,10 +1,36 @@
-var a = [];
-a[0] = 'hello';
-console.log(a.length);
+// var a = [];
+// a[0] = 'hello';
+// console.log(a.length);
+//
+// a['12'] = '哈哈';
+// console.log(a.length);
+//
+// var d = new Date();
+// d['first']='1';
+// console.log(d.first);
 
-a['12'] = '哈哈';
-console.log(a.length);
+// var str = 'hello';
+// var arr = ['h', 'e', 'l', 'l', 'o'];
+//
+// str[1] = 'a';
+// arr[1] = 'a';
+//
+// console.log(str);
+// console.log(arr);
+//
+// var str2 = str.toUpperCase();
+// console.log(str);
+// console.log(str2);
 
-var d = new Date();
-d['first']='1';
-console.log(d.first);
+// var str = 'hello';
+//
+// console.log(Array.prototype.join.call(str, '-'));
+//
+// var res = Array.prototype.map.call(str,function(v){
+//     return v.toUpperCase() + '.';
+// }).join('');
+// console.log(res);
+
+var str = 'hello';
+var reversedStr = str.split('').reverse().join('');
+console.log(reversedStr);
